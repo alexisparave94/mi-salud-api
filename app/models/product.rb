de1f82bdc9aa2932901bc46512bbcdcb8d78646d
Alexis Parave <aparavev@gmail.com>
2022-07-23 14:10:53 -0500

    FEAT: add validations to products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  # Validations
+  validates :cod_product, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :sale_price, numericality: { greater_than_or_equal_to: 0 }
 end
