b0a4592f46717aa1676f819d9d817891deed9b95
Alexis Parave <aparavev@gmail.com>
2022-07-23 14:37:16 -0500

    FEAT: add list of laboratories

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
   validates :sale_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :laboratory, inclusion: { in: %w[Genfar GSK Hersil FarmaIndustria] }
 end
